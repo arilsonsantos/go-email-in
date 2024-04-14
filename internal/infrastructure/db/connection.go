44bbe63ec0f7bac80c876383d9fefa595e596026
Arilson Santos <arilsonsantos@gmail.com>
2024-04-14 19:52:28 -0300

    Refatorado

--- internal/infrastructure/db/connection.go
+++ internal/infrastructure/db/connection.go
@@ -1,12 +1,9 @@
 package db
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	//_ "github.com/mattn/go-sqlite3"
-	//_ "github.com/jackc/pgx/v5"
 )
 
 type DB struct {
@@ -16,7 +13,6 @@ type DB struct {
 var dbConn = &DB{}
 
 func OpenConn() (*DB, error) {
-	//sc := "database.db"
 	strDb := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
@@ -38,14 +34,4 @@ func OpenConn() (*DB, error) {
 
 	fmt.Println("Conexão com o banco de dados SQLite estabelecida com sucesso!")
 	return dbConn, err
-}
-
-func testDB(conn *sql.DB) error {
-	err := conn.Ping()
-	if err != nil {
-		fmt.Println("Erro ao conectar ao banco de dados:", err)
-		return err
-	}
-	fmt.Println("Conexão com o banco de dados SQLite estabelecida com sucesso!")
-	return nil
 }
